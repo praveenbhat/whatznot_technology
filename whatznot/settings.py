42387f29001b8291c42f0125b4a7a4ad047a859f
hellopritammandal <pmihub@gmail.com>
2023-01-15 01:05:27 +0530

    try

--- whatznot/settings.py
+++ whatznot/settings.py
@@ -338,10 +338,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'assets']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'assets'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
