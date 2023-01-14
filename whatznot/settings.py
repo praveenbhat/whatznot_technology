d45816b7cb9064f5937ae2abb0057cb89dc7e456
hellopritammandal <pmihub@gmail.com>
2023-01-15 02:43:13 +0530

    commit16

--- whatznot/settings.py
+++ whatznot/settings.py
@@ -203,7 +203,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nw#@u*$=rslp9dwexakq2=4r*kb@a%(f4nrb5jmk0jz=t8fgxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -345,7 +345,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
