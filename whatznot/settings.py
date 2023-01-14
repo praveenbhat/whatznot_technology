6fc43ddac6f7b1cc2519f5382a3271cedcd0090c
hellopritammandal <pmihub@gmail.com>
2023-01-15 02:21:28 +0530

    commit14

--- whatznot/settings.py
+++ whatznot/settings.py
@@ -339,9 +339,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
