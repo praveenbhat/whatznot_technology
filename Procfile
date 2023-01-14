022e92c97433b5544181b40a96066bdbeea561e1
hellopritammandal <pmihub@gmail.com>
2023-01-14 23:54:02 +0530

    final

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn whatznot.wsgi --log-file -
--- whatznot/settings.py
+++ whatznot/settings.py
@@ -205,7 +205,7 @@ SECRET_KEY = 'django-insecure-nw#@u*$=rslp9dwexakq2=4r*kb@a%(f4nrb5jmk0jz=t8fgxx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
