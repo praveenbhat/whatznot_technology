3c88b9db63976c16c3167dc243564b3333bb135f
hellopritammandal <pmihub@gmail.com>
2023-01-15 06:21:16 +0530

    final try6 15-01-2023

--- whatznot/settings.py
+++ whatznot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nw#@u*$=rslp9dwexakq2=4r*kb@a%(f4nrb5jmk0jz=t8fgxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
